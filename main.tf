a548e9052a14e18e894106d07c646e07ba08db46
pswainu <prasanjit412@gmail.com>
2022-02-07 12:06:42 +0530

    4

--- main.tf
+++ main.tf
@@ -2,14 +2,3 @@ provider "google" {
   project = var.projectid
   region = var.region
 }
-
-/*
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-  required_version = ">= 0.13"
-}
-*/
